e81a74fb817a125e92abc1e740535f9a3836152c
Vic Iglesias <viglesias@google.com>
2016-12-01 21:50:01 -0800

    Reorg tests (#57)
    * Add test tasks and scripts
    
    * Reorg tests

---
+++ tests/scripts/cleanup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -xe
+printf "y\n" | gcloud container clusters delete jenkins-cd || true
+printf "y\n" | gcloud compute images delete jenkins-home-image || true
+printf "y\n" | gcloud compute disks delete jenkins-home || true
+for rule in $(gcloud compute firewall-rules list --filter network=jenkins --format='value(name)');do
+  printf "y\n" | gcloud compute firewall-rules delete $rule || true
+done
+for rule in $(gcloud compute forwarding-rules list  --regexp '.*jenkins-jenkins.*'  --format='value(name)');do
+  printf "y\n" | gcloud compute forwarding-rules delete $rule --global
+done
+for address in $(gcloud compute addresses list --regexp '.*-jenkins-jenkins.*'  --format='value(name)');do
+  printf "y\n" | gcloud compute addresses delete $address --global
+done
+for proxy in $(gcloud compute target-https-proxies list  --regexp '.*jenkins-jenkins.*'  --format='value(name)');do
+  printf "y\n" | gcloud compute target-https-proxies delete $proxy
+done
+for cert in $(gcloud compute ssl-certificates list --regexp 'k8s-ssl-jenkins-jenkins.*'  --format='value(name)');do
+  printf "y\n" | gcloud compute ssl-certificates delete $cert
+done
+for target in $(gcloud compute target-pools list --regexp '.*-jenkins-jenkins.*'  --format='value(name)');do
+  printf "y\n" | gcloud compute target-pools delete $target
+done
+for target in $(gcloud compute target-http-proxies list --regexp '.*-jenkins-jenkins.*'  --format='value(name)');do
+  printf "y\n" | gcloud compute target-http-proxies delete $target
+done
+for url in $(gcloud compute url-maps list --regexp '.*-jenkins-jenkins.*'  --format='value(name)');do
+  printf "y\n" | gcloud compute url-maps delete $url
+done
+for backend in $(gcloud compute backend-services list --regexp '.*-jenkins-jenkins.*'  --format='value(name)');do
+  printf "y\n" | gcloud compute backend-services delete $backend || true
+done
+printf "y\n" | gcloud compute networks delete jenkins || true
