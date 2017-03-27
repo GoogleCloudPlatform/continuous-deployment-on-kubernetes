d4d89214e980e35262cb5ea8eebd15ae6affe03d
Vic Iglesias <viglesias@google.com>
2017-03-27 14:40:55 -0700

    Give ingress 10 minutes to go to HEALTHY

--- tests/run-tutorial.sh
+++ tests/run-tutorial.sh
@@ -17,7 +17,7 @@ kubectl get svc --namespace jenkins
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/tls.key -out /tmp/tls.crt -subj "/CN=jenkins/O=jenkins"
 kubectl create secret generic tls --from-file=/tmp/tls.crt --from-file=/tmp/tls.key --namespace jenkins
 kubectl apply -f jenkins/k8s/lb/ingress.yaml
-for i in `seq 1 5`;do kubectl describe ingress jenkins --namespace jenkins; sleep 60;done
+for i in `seq 1 10`;do kubectl describe ingress jenkins --namespace jenkins; sleep 60;done
 
 kubectl get ingress --namespace jenkins -o jsonpath='{.status.loadBalancer.ingress[0].ip}' jenkins
 kubectl describe ingress --namespace=jenkins jenkins | grep backends | grep HEALTHY
