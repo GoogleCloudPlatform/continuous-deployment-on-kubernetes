96a1f1ee3bb015d717d2ca34340b3d797a61a96f
Ricardo Ferreira <ricardo.sff@gmail.com>
2017-09-20 01:25:20 +0100

    added basic OS detection

--- tests/scripts/install-jenkins.sh
+++ tests/scripts/install-jenkins.sh
@@ -1,4 +1,21 @@
 #!/bin/bash -xe
+
+case "$OSTYPE" in
+  darwin*)  OS="darwin" ENDIAN="amd64" ;;
+  linux*)   OS="linux" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
+
+if [ $OS = "linux" ] && [ `getconf LONG_BIT` == 64 ]; then
+        ENDIAN="amd64"
+elif [ `getconf LONG_BIT` == 32 ]; then
+        ENDIAN="386"
+else
+        #assume amd64"
+        ENDIAN="amd64"
+fi
+
+
 gcloud compute networks create jenkins --mode auto
 gcloud container clusters create jenkins-cd \
         --machine-type n1-standard-2 \
@@ -10,9 +27,9 @@ gcloud container clusters get-credentials jenkins-cd
 kubectl cluster-info
 
 HELM_VERSION=2.6.1
-wget https://storage.googleapis.com/kubernetes-helm/helm-v$HELM_VERSION-linux-amd64.tar.gz
-tar zxfv helm-v$HELM_VERSION-linux-amd64.tar.gz
-cp linux-amd64/helm .
+wget https://storage.googleapis.com/kubernetes-helm/helm-v$HELM_VERSION-$OS-$ENDIAN.tar.gz
+tar zxfv helm-v$HELM_VERSION-$OS-$ENDIAN.tar.gz
+cp $OS-$ENDIAN/helm .
 ./helm init
 ./helm repo update
 # Give tiller a chance to start up
