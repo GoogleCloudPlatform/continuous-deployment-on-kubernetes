349ec9e39c1cefa806249db27613d54b7b140ee9
Vic Iglesias <viglesias@google.com>
2018-07-10 22:47:18 -0700

    Update install-jenkins test

--- tests/scripts/install-jenkins.sh
+++ tests/scripts/install-jenkins.sh
@@ -9,23 +9,27 @@ gcloud container clusters list
 gcloud container clusters get-credentials jenkins-cd
 kubectl cluster-info
 
-HELM_VERSION=2.6.1
+HELM_VERSION=2.9.1
 wget https://storage.googleapis.com/kubernetes-helm/helm-v$HELM_VERSION-linux-amd64.tar.gz
 tar zxfv helm-v$HELM_VERSION-linux-amd64.tar.gz
 cp linux-amd64/helm .
-./helm init
-./helm repo update
-# Give tiller a chance to start up
-# TODO: Change this to polling
-sleep 180
-./helm version | grep $HELM_VERSION
 
-./helm install -n cd stable/jenkins -f jenkins/config.yaml --version 0.8.9
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
+
+kubectl create serviceaccount tiller --namespace kube-system
+kubectl create clusterrolebinding tiller-admin-binding --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+
+./helm init --service-account=tiller
+./helm update
+
+# Give tiller a chance to start up
+until ./helm version; do sleep 10;done
+
+./helm install -n cd stable/jenkins -f jenkins/values.yaml --version 0.16.6 --wait
 
 for i in `seq 1 5`;do kubectl get pods; sleep 60;done
 
-kubectl get pods -l app=cd-jenkins | grep Running
+until kubectl get pods -l app=cd-jenkins | grep Running; do sleep 10;done
 
 # Cleanup resources
 ./helm delete --purge cd
-sleep 120
