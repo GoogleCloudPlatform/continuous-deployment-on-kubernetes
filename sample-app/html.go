c667a5d5c885c38e9db903294c5f3e0e89f4f2a8
XOXOT <1418083@donga.ac.kr>
2022-11-16 06:00:09 +0000

    html.go update

--- sample-app/html.go
+++ sample-app/html.go
@@ -34,7 +34,7 @@ const (
 <div class="col s8">
 
 
-<div class="card blue">
+<div class="card green">
 <div class="card-content white-text">
 <div class="card-title">Backend that serviced this request</div>
 </div>
